cb695c4255999f0b22c0ea9a30bf33f3437dfe37
arglp <leopold.prueller@gmx.at>
2024-12-13 19:45:14 +0100

    Starting to work on system

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-from textnode import *
+import os
+import shutil
 
+
+current_path = os.getcwd()
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    source_dir = "static"
+    destination_dir = "public"
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+    os.mkdir(destination_dir)
 
 if __name__ == "__main__":
     main()
+
+
